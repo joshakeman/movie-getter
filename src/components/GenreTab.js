ae74aad307ebb1922ddc1ae71e687f21083ebf09
Josh Akeman <joshakeman@gmail.com>
2019-11-10 18:40:17 -0500

    made it purdier

--- src/components/GenreTab.js
+++ src/components/GenreTab.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-export default function GenreTab(props) {
-    return (
-        <button onClick={()=> props.getMovieList(props.id)}>
-            {props.name}
-        </button>
-    )
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function OutlinedButtons(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button variant="outlined" className={classes.button} onClick={()=> props.getMovieList(props.id)}>
+        {props.name}
+      </Button>
+    </div>
+  );
 }
